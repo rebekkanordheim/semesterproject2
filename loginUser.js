0b1952d597ab1aff024d63f734ecb2c6738c7638
rebekkanordheim <RebNor46673@stud.noroff.no>
2023-11-27 14:38:18 +0100

    login and register function done

---
+++ loginUser.js
@@ -0,0 +1,74 @@
+// function to handle the login form submission
+function handleLogin(event) {
+    event.preventDefault();
+
+    // select the form elements
+    const emailInput = document.getElementById('loginEmail');
+    const passwordInput = document.getElementById('loginPassword');
+    const loginForm = document.getElementById('loginForm');
+
+    // validating the form inputs
+    if (emailInput.value.match(/^[\w\-.]+@stud\.noroff\.no$/) && passwordInput.value.length >= 8) {
+        // if the inputs are valid, login the user
+        loginUser(emailInput.value, passwordInput.value);
+        // close the form and reset the values
+        loginForm.reset();
+        const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
+        loginModal.hide();
+    } else {
+        alert('Email must be a valid Noroff student email, and password must be 8 characters long.');
+    }
+}
+
+// Add a click event listener to the login button
+const loginButton = document.getElementById('loginButton');
+loginButton.addEventListener('click', handleLogin);
+
+
+/// Function to log in the user
+function loginUser(email, password) {
+    const loginUrl = 'https://api.noroff.dev/api/v1/auction/auth/login';
+    const userData = {
+        email: email,
+        password: password
+    };
+
+    try {
+        // Do API call
+        const postData = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        };
+
+        fetch(loginUrl, postData)
+            .then(response => {
+                if (response.ok) {
+                    // Login successful
+                    console.log('Login successful! Email:', email);
+                    document.getElementById('loginSuccessMessage').textContent = 'Login successful! Email: ' + email;
+                    return response.json();
+                } else {
+                    throw new Error('Login failed. Please check your email and password, and ensure you have registered first.');
+                }
+            })
+            .then(data => {
+                // Assuming the API returns a token property in the response
+                const token = data.accessToken;
+                // Saving the JWT token to local storage
+                localStorage.setItem('jwtToken', token);
+                // Display a success message
+                alert('Login successful! Email: ' + email);
+            })
+            .catch(error => {
+                // Handle login error
+                console.error('Error during login', error);
+                alert('An error occurred. Please try again later.');
+            });
+    } catch (error) {
+        console.error('Unexpected error during login', error);
+        alert('An unexpected error occurred. Please try again later.');
+    }
+}
---
+++ registerUser.js
@@ -0,0 +1,146 @@
+// Function to handle form submission
+function handleRegistration(event) {
+    event.preventDefault();
+
+    // Select form elements
+    const nameInput = document.getElementById('registerName');
+    const emailInput = document.getElementById('registerEmail');
+    const passwordInput = document.getElementById('registerPassword');
+    const avatarInput = document.getElementById('registerAvatar');
+    const registerForm = document.getElementById('registerForm');
+
+    // Validate form inputs
+    if (
+        nameInput.value.length >= 1 &&
+        passwordInput.value.length >= 8 &&
+        emailInput.value.match(/^[\w\-.]+@stud\.noroff\.no$/) &&
+        avatarInput.value.trim() !== ''
+    ) {
+        // If inputs are valid, register the user
+        registerUser(
+            nameInput.value,
+            emailInput.value,
+            passwordInput.value,
+            avatarInput.value
+        );
+
+        // Close the form and reset values
+        registerForm.reset();
+        const registerModal = new bootstrap.Modal(document.getElementById('registerModal'));
+        registerModal.hide();
+
+    } else {
+        alert('Name must be at least 1 character long. Password must be 8 characters long, and email must be a valid Noroff student email, and remember an avatar image URL');
+    }
+}
+const registerButton = document.getElementById('registerButton');
+registerButton.addEventListener('click', handleRegistration);
+
+
+// Function to register the user
+async function registerUser(name, email, password, avatar) {
+    const registerUrl = 'https://api.noroff.dev/api/v1/auction/auth/register';
+
+    // Validate form inputs
+    if (
+        name.length >= 1 &&
+        password.length >= 8 &&
+        email.match(/^[\w\-.]+@stud\.noroff\.no$/) &&
+        avatar.trim() !== ''
+    ) {
+        const userData = {
+            name: name,
+            email: email,
+            password: password,
+            avatar: avatar
+        };
+
+        try {
+            // do API call
+            const postData = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            };
+
+            const response = await fetch(registerUrl, postData);
+
+            if (response.ok) {
+                // Registration successful, get JWT token from the response
+                const data = await response.json();
+                const token = data.accessToken;
+
+                // Store the token in local storage
+                localStorage.setItem('jwtToken', token);
+
+                // Log name and email to console
+                console.log('Name:', name);
+                console.log('Email:', email);
+
+                console.log('Registration successful! Token:', token);
+                // You might want to redirect the user or perform other actions
+            } else {
+                // Handle registration error
+                console.error('Registration failed. Please try again.');
+                alert('Registration failed. Please try again.');
+            }
+        } catch (error) {
+            // Handle fetch error
+            console.error('Error during registration:', error);
+            alert('An error occurred. Please try again later.');
+        }
+    } else {
+        // Handle validation errors
+        alert('Name must be at least 1 character long. Password must be 8 characters long, and email must be a valid Noroff student email, and remember an avatar image URL');
+    }
+}
+
+
+/* function registerUser(name, email, password, avatar) {
+    const registerUrl = 'https://api.noroff.dev/api/v1/auction/auth/register';
+
+    const userData = {
+        name: name,
+        email: email,
+        password: password,
+        avatar: avatar
+    };
+
+    const fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    };
+
+    fetch(registerUrl, fetchOptions)
+        .then(response => {
+            if (response.ok) {
+                // Registration successful, get JWT token from the response
+                return response.json();
+            } else {
+                throw new Error('Registration failed. Please try again.');
+            }
+        })
+        .then(data => {
+            // Assuming the API returns a token property in the response
+            const token = data.accessToken;
+
+            // Store the token in local storage
+            localStorage.setItem('jwtToken', token);
+
+            // Log name and email to console
+            console.log('Name:', name);
+            console.log('Email:', email);
+
+            console.log('Registration successful! Token:', token);
+            // You might want to redirect the user or perform other actions
+        })
+        .catch(error => {
+            console.error('Error during registration:', error);
+            alert(error.message || 'An error occurred. Please try again later.');
+        });
+} */
