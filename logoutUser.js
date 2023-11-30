a831368697e8487b9d9de0c68bbe53d8baa4ed00
rebekkanordheim <RebNor46673@stud.noroff.no>
2023-11-30 11:03:54 +0100

    logout function works

---
+++ logoutUser.js
@@ -0,0 +1,22 @@
+// Function to log out the user
+function logoutUser() {
+    // Remove the JWT token from local storage
+    localStorage.removeItem('jwtToken');
+    
+    // Log a message to the console
+    console.log('The user has been logged out');
+
+    // Toggle visibility of buttons
+    const registerButton = document.getElementById('createProfileButton');
+    const loginButton = document.getElementById('loginButton');
+    const logoutButton = document.getElementById('logoutButton');
+
+    // Show create profile and login buttons, hide logout button
+    registerButton.style.display = 'inline-block';
+    loginButton.style.display = 'inline-block';
+    logoutButton.style.display = 'none';
+}
+
+// Add a click event listener to the logout button
+const logoutButton = document.getElementById('logoutButton');
+logoutButton.addEventListener('click', logoutUser);
