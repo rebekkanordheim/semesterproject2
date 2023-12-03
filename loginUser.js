7196ecadd3d64b089082b47b2bc2d41ac7630337
rebekkanordheim <RebNor46673@stud.noroff.no>
2023-12-03 11:54:00 +0100

    login form bug fixed

--- loginUser.js
+++ loginUser.js
@@ -1,114 +1,25 @@
-/* // function to handle the login form submission
-function handleLogin(event) {
-    event.preventDefault();
-
-    // select the form elements
-    const emailInput = document.getElementById('loginEmail');
-    const passwordInput = document.getElementById('loginPassword');
-    const loginForm = document.getElementById('loginForm');
-
-    // validating the form inputs
-    if (emailInput.value.match(/^[\w\-.]+@stud\.noroff\.no$/) && passwordInput.value.length >= 8) {
-        // if the inputs are valid, login the user
-        loginUser(emailInput.value, passwordInput.value);
-        // close the form and reset the values
-        loginForm.reset();
-
-        // Hide the login modal
-        const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
-        loginModal.hide();
-
-        // Optionally, you can close or remove the login form as well
-        loginForm.style.display = 'none'; // or loginForm.remove();
-
-        // Optionally, you can hide the modal backdrop as well
-        const modalBackdrops = document.getElementsByClassName('modal-backdrop');
-        for (const backdrop of modalBackdrops) {
-            backdrop.style.display = 'none';
-        }
-    } else {
-        alert('Email must be a valid Noroff student email, and password must be 8 characters long.');
-    }
-}
-
-// Add a click event listener to the login button
-const loginButton = document.getElementById('loginButton');
-loginButton.addEventListener('click', handleLogin);
-
-
-// Function to log in the user
-function loginUser(email, password) {
-    const loginUrl = 'https://api.noroff.dev/api/v1/auction/auth/login';
-    const userData = {
-        email: email,
-        password: password
-    };
-
-    try {
-        // Do API call
-        const postData = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userData),
-        };
-
-        fetch(loginUrl, postData)
-            .then(response => {
-                if (response.ok) {
-                    // Login successful
-                    console.log('Login successful! Email:', email);
-                    document.getElementById('loginSuccessMessage').textContent = 'Login successful! Email: ' + email;
-                    return response.json();
-                } else {
-                    throw new Error('Login failed. Please check your email and password, and ensure you have registered first.');
-                }
-            })
-            .then(data => {
-                // Assuming the API returns a token property in the response
-                const token = data.accessToken;
-                // Saving the JWT token to local storage
-                localStorage.setItem('jwtToken', token);
-                // Display a success message
-                alert('Login successful! Email: ' + email);
-            })
-            .catch(error => {
-                // Handle login error
-                console.error('Error during login', error);
-                alert('An error occurred. Please try again later.');
-            });
-    } catch (error) {
-        console.error('Unexpected error during login', error);
-        alert('An unexpected error occurred. Please try again later.');
-    }
-} */
-
-// function to handle the login form submission
-function handleLogin(event) {
+// Function to handle the login form submission
+async function handleLogin(event) {
     event.preventDefault();
 
     const emailInput = document.getElementById('loginEmail');
     const passwordInput = document.getElementById('loginPassword');
     const loginForm = document.getElementById('loginForm');
+    const closeModalButton = document.querySelector('#loginModal .btn-close');
 
-    // validating the form inputs
+    // Validating the form inputs
     if (emailInput.value.match(/^[\w\-.]+@stud\.noroff\.no$/) && passwordInput.value.length >= 8) {
-        loginUser(emailInput.value, passwordInput.value);
-        loginForm.reset();
-
-        // Hide the login modal
-        const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
-        loginModal.hide();
-
-        loginForm.style.display = 'none'
-
-        const modalBackdrops = document.getElementsByClassName('modal-backdrop');
-        for (const backdrop of modalBackdrops) {
-            backdrop.style.display = 'none';
+        try {
+            await loginUser(emailInput.value, passwordInput.value);
+            loginForm.style.display = 'none'; // Hide the entire login form
+            alert('Login successful! Email: ' + emailInput.value);
+            closeModalButton.click(); // Simulate a click on the close button
+        } catch (error) {
+            console.error('Error during login', error);
+            alert('An error occurred. Please try again later.');
         }
     } else {
-        alert('Email must be a valid Noroff student email, and password must be 8 characters long.');
+        alert('Email must be a valid Noroff student email, and the password must be 8 characters long.');
     }
 }
 
