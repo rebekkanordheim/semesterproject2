a307fc57da1c1439f24dc521646be21f0fe7efa1
rebekkanordheim <RebNor46673@stud.noroff.no>
2023-12-07 16:27:24 +0100

    cleaning up

--- js/listings.js
+++ js/listings.js
@@ -13,51 +13,6 @@ async function getListings() {
 async function showListings() {
     const listings = await getListings();
     console.log(listings);
-}
-showListings();
-
-// Creating the HTML for each post that gets called from the API
-function createListingsHTML(listings) {
-    const container = document.querySelector('card-content');
-
-    listings.forEach(listing => {
-        // Create the listing container 
-        const listingsContainer = document.createElement('div');
-        listingsContainer.classList.add('product-cards');
-        listingsContainer.id = listing.id;
-
-        // Create the listing title
-        const title = document.createElement('h3');
-        title.innerText = listing.title.rendered;
-        listingsContainer.append(title);
-
-        // Create the listing content
-        const content = document.createElement('div');
-        content.innerHTML = listing.content.rendered;
-        listingsContainer.append(content);
-
-        container.append(listingsContainer);
-    });
-}
- */
-
-
-
-// Fetching from the API
-async function getListings() {
-    const response = await fetch('https://api.noroff.dev/api/v1/auction/listings?_bids=true');
-    
-    if (!response.ok) {
-        throw new Error('Failed to fetch listings');
-    }
-    
-    return await response.json();
-}
-
-// Console logging to check that the fetch works
-async function showListings() {
-    const listings = await getListings();
-    console.log(listings);
     createListingsHTML(listings);
 }
 
@@ -92,7 +47,7 @@ function createListingsHTML(listings) {
         // Create button to get to each listings own site
         const button = document.createElement('button');
         button.type = 'button';
-        button.className = 'actionButtonOne cardButton';
+        button.className = 'btn-outline-success cardButton';
         button.innerText = 'VIEW';
         button.onclick = function () {
             window.location.href = `html/${listing.id}.html`;
@@ -105,4 +60,92 @@ function createListingsHTML(listings) {
 
 document.addEventListener('DOMContentLoaded', function () {
     showListings();
+}); */
+
+// listings.js
+document.addEventListener('DOMContentLoaded', async function () {
+    try {
+        const listings = await getListings();
+        console.log(listings);
+        createListingsHTML(listings);
+
+        // Check if the user is logged in (update this logic accordingly)
+        const isLoggedIn = /* Add your logic to check if the user is logged in */ true;
+
+        // Show bid form for logged-in users
+        if (isLoggedIn) {
+            showBidForm();
+        }
+    } catch (error) {
+        console.error('Error fetching listings:', error.message);
+    }
 });
+
+// Fetching from the API
+async function getListings() {
+    const response = await fetch('https://api.noroff.dev/api/v1/auction/listings?_bids=true');
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch listings');
+    }
+    return await response.json();
+}
+
+// Creating the HTML for each listing
+function createListingsHTML(listings) {
+    const container = document.querySelector('.card-content');
+
+    listings.forEach(listing => {
+        const listingsContainer = document.createElement('div');
+        listingsContainer.classList.add('product-cards');
+
+        const title = document.createElement('h2');
+        title.innerText = listing.title;
+        listingsContainer.append(title);
+
+        const image = document.createElement('img');
+        if (listing.media && listing.media[0]) {
+            image.src = listing.media[0].url;
+        } else {
+            image.src = 'images/BidWaveLogo1.png';
+        }
+        image.alt = listing.title;
+        image.classList.add('card-image');
+        listingsContainer.append(image);
+
+        const bidForm = document.createElement('form');
+        bidForm.classList.add('bid-form');
+        bidForm.innerHTML = `
+            <input class='form-control form-control-dark' type="number" id="bidAmount" name="bidAmount" placeholder="Your bid"required>
+
+            <button type="button" class=" cardButton" onclick="addBid(${listing.id})">Add Bid</button>
+        `;
+        listingsContainer.append(bidForm);
+
+        container.append(listingsContainer);
+    });
+}
+
+// Function to show bid form
+function showBidForm() {
+    const bidForms = document.querySelectorAll('.bid-form');
+    bidForms.forEach(form => {
+        form.style.display = 'block';
+    });
+}
+
+// Function to add a bid (you may customize this logic)
+function addBid(listingId) {
+    const bidAmount = document.getElementById('bidAmount').value;
+    console.log(`Adding bid of ${bidAmount} for listing ${listingId}`);
+
+    // Add your logic to handle the bid
+
+    // Show success message
+    const successMessage = document.getElementById('bidSuccessMessage');
+    successMessage.innerText = 'Your bid has been placed!';
+    // You can add additional logic to hide the message after a certain time
+    setTimeout(() => {
+        successMessage.innerText = ''; // Clear the message
+    }, 3000); // Display for 3 seconds (adjust as needed)
+}
