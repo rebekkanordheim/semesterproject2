a4473a84964f33820d8c8e3987b9bb5d0ab86a14
rebekkanordheim <RebNor46673@stud.noroff.no>
2023-12-07 14:46:39 +0100

    saving listings.js - works

--- js/listings.js
+++ js/listings.js
@@ -46,11 +46,19 @@ function createListingsHTML(listings) {
 // Fetching from the API
 async function getListings() {
     const response = await fetch('https://api.noroff.dev/api/v1/auction/listings?_bids=true');
-
+    
     if (!response.ok) {
         throw new Error('Failed to fetch listings');
     }
+    
     return await response.json();
+}
+
+// Console logging to check that the fetch works
+async function showListings() {
+    const listings = await getListings();
+    console.log(listings);
+    createListingsHTML(listings);
 }
 
 // Creating the HTML for each listing
@@ -61,24 +69,31 @@ function createListingsHTML(listings) {
         // Create the listing container 
         const listingsContainer = document.createElement('div');
         listingsContainer.classList.add('product-cards');
+        listingsContainer.id = listing.id;
 
         // Create the listing title
-        const title = document.createElement('h2');
+        const title = document.createElement('h3');
         title.innerText = listing.title;
         listingsContainer.append(title);
 
         // Create the listing image
         const image = document.createElement('img');
-        image.src = listing.media && listing.media[0] ? listing.media[0].url : 'images/BidWaveLogo1.png';
         image.alt = listing.title;
         image.classList.add('card-image');
+        // Check if listing has media and the first media item has a URL
+        if (listing.media && listing.media[0] && listing.media[0].url) {
+            image.src = listing.media[0].url;
+        } else {
+            // Using a placeholder image if the URL is undefined
+            image.src = '../images/BidWaveLogo1.png';
+        }
         listingsContainer.append(image);
 
         // Create button to get to each listings own site
         const button = document.createElement('button');
         button.type = 'button';
         button.className = 'actionButtonOne cardButton';
-        button.innerText = 'Show';
+        button.innerText = 'VIEW';
         button.onclick = function () {
             window.location.href = `html/${listing.id}.html`;
         };
@@ -86,13 +101,6 @@ function createListingsHTML(listings) {
 
         container.append(listingsContainer);
     });
-}
-
-// Console logging to check that the fetch works
-async function showListings() {
-    const listings = await getListings();
-    console.log(listings);
-    createListingsHTML(listings);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
